e87ff2a30105cc032a66ae808da7d31b4e063893
klug0 <47000041+luit0r@users.noreply.github.com>
2019-03-22 22:40:57 +0530

    Add files via upload
    this bash script is made to run in arch based distros!

---
+++ archin.sh
@@ -0,0 +1,19 @@
+printf "\e[1;92m__________________________________\n\e[0m"
+printf "\e[1;92m                                  \n\e[0m"
+
+printf "\e[1;92m PROJECT HAXTAGRAM!\n\e[0m"
+printf "\e[1;92m__________________________________\n\e[0m"
+printf "\e[1;92mA Bruteforce tool made by luit0r (ORIGINALABHAY)\n\e[0m"
+
+
+
+#install curl
+sudo pacman -Sy install curl
+
+#install TOR
+sudo pacman -Sy install tor
+
+#install openSSL
+sudo pacman -Sy install openssl
+printf "\e[1;92m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\e[0m"
+printf "\e[1;92mALL FILES ARE INSTALLED\n\e[0m"
